4f08d2eb66d87a7c431f351da7f106a9c7f1b95d
Rolf Becker <rb@hsrw.eu>
2021-02-02 21:03:33 +0100

    geo0810 added.

---
+++ geo0810_DWD_Station_Layer_Creation_for_QGIS/my_dwd.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import os
+import ftplib
+
+
+def grabFile(ftp,ftpfullname,localfullname):
+    try:
+        ret = ftp.cwd(".") # A dummy action to check the connection and to provoke an exception if necessary.
+        localfile = open(localfullname, 'wb')
+        ftp.retrbinary('RETR ' + ftpfullname, localfile.write, 1024)
+        localfile.close()
+    
+    except ftplib.error_perm:
+        print("FTP ERROR. Operation not permitted. File not found?")
+
+    except ftplib.error_temp:
+        print("FTP ERROR. Timeout.")
+
+    except ConnectionAbortedError:
+        print("FTP ERROR. Connection aborted.")
+
+
+        
+# generate a pandas dataframe from a FTP directory listing. 
+def gen_df_from_ftp_dir_listing(ftp, ftpdir):
+    lines = []
+    flist = []
+    try:
+        # issue the command LIST in the FTP connection 
+        res = ftp.retrlines("LIST "+ftpdir, lines.append)
+    except:
+        print("Error: ftp.retrlines() failed. ftp timeout? Reconnect!")
+        return
+        
+    if len(lines) == 0:
+        print("Error: ftp dir is empty")
+        return
+    
+    for line in lines:
+#        print(line)
+        [ftype, fsize, fname] = [line[0:1], int(line[31:42]), line[56:]]
+        
+        fext = os.path.splitext(fname)[-1]
+        
+        if fext == ".zip":
+            station_id = int(fname.split("_")[2])
+        else:
+            station_id = -1 
+        
+        flist.append([station_id, fname, fext, fsize, ftype])
+        
+    df_ftpdir = pd.DataFrame(flist,columns=["station_id", "name", "ext", "size", "type"])
+    return(df_ftpdir)
+
+
+# extract column names. They are in German (de)
+# We have to use codecs because of difficulties with character encoding (German Umlaute)
+import codecs
+
+def station_desc_txt_to_csv(txtfile, csvfile):
+    import pandas as pd
+    
+    file = codecs.open(txtfile,"r","utf-8")
+    r = file.readline()
+    file.close()
+    colnames_de = r.split()
+    colnames_de
+    
+    # German-English dictionary
+    translate = \
+    {'Stations_id':'station_id',
+     'von_datum':'date_from',
+     'bis_datum':'date_to',
+     'Stationshoehe':'altitude',
+     'geoBreite': 'latitude',
+     'geoLaenge': 'longitude',
+     'Stationsname':'name',
+     'Bundesland':'state'}
+    
+    colnames_en = [translate[h] for h in colnames_de]
+    
+    # Skip the first two rows and set the column names.
+    df = pd.read_fwf(txtfile,skiprows=2,names=colnames_en, parse_dates=["date_from","date_to"],index_col = 0)
+    
+    # write CSV file with field separator semicolon
+    df.to_csv(csvfile, sep = ";")
+    return(df)
+
+
+# Filter to extract PRECIPITATION from recent and historical RR product files.
+
+def prec_ts_to_df(fname):
+
+    import pandas as pd
+    import pytz
+    from datetime import datetime
+    
+    dateparse = lambda dates: [datetime.strptime(str(d), '%Y%m%d%H') for d in dates]
+
+    df = pd.read_csv(fname, delimiter=";", encoding="utf8", index_col="MESS_DATUM", parse_dates = ["MESS_DATUM"], date_parser = dateparse, na_values = [-999.0, -999])
+
+    #df = pd.read_csv(fname, delimiter=";", encoding="iso8859_2",\
+    #             index_col="MESS_DATUM", parse_dates = ["MESS_DATUM"], date_parser = dateparse)
+    
+    # https://medium.com/@chaimgluck1/working-with-pandas-fixing-messy-column-names-42a54a6659cd
+
+    # Column headers: remove leading blanks (strip), replace " " with "_", and convert to lower case.
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+    df.index.name = df.index.name.strip().lower().replace(' ', '_').replace('(', '').replace(')', '')
+    
+    # TIME ZONES: https://stackoverflow.com/questions/22800079/converting-time-zone-pandas-dataframe
+    # DWD Prec Data is given in UTC
+    
+    df.index = df.index.tz_localize(pytz.utc)
+    
+    return(df)
